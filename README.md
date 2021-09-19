9ef75cf02bc59c03269219a8c84b5eaa345cde29
danielsmith0630 <danielsmith0630@gmail.com>
2021-09-19 08:01:50 -0500

    added all functionalities

---
+++ README.md
@@ -0,0 +1,27 @@
+# React Bootstrap Boilerplate
+
+## Install
+
+```sh
+$ npm install
+or
+$ yarn install
+```
+
+## Develop
+
+```sh
+$ npm start
+or
+$ yarn start
+```
+
+## Production
+
+```sh
+$ npm build
+or
+$yarn build
+```
+
+## Deployment
---
+++ src/js/layouts/Full/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Container
+} from 'reactstrap';
+
+export const FooterIntro = () => (
+  <div className="site-footer">
+    <Container>
+      Footer Part here
+    </Container>
+  </div>
+);
+
+export default FooterIntro;
